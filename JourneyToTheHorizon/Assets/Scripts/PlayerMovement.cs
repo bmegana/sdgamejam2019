6bd32ac743e845b9f61402f32819eb7a79c319c7
Dana Kimball <dkimball@ucsd.edu>
2019-07-27 16:27:09 -0700

    Improved Player Movement

--- JourneyToTheHorizon/Assets/Scripts/PlayerMovement.cs
+++ JourneyToTheHorizon/Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,17 @@
 public class PlayerMovement : MonoBehaviour
 {
     public float maxSpeed;
+	public float angularSpeed;
 
     private Rigidbody rb;
+
+	public Transform cameraTransform;
+	private Quaternion originalCameraRotation;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+		originalCameraRotation = cameraTransform.rotation;
     }
 
     private void Update()
@@ -17,10 +22,25 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 dir = new Vector3(
-            horizontal * maxSpeed, rb.velocity.y, vertical * maxSpeed
-        );
-        
-        rb.velocity = dir;
+		if ( horizontal != 0 || vertical != 0 )
+		{
+			Vector3 dir = new Vector3( horizontal, 0.0f, vertical );
+			dir.Normalize();
+
+			Quaternion targetRotation = Quaternion.LookRotation( dir, Vector3.up );
+
+			Quaternion newRotation = Quaternion.RotateTowards( transform.rotation, targetRotation, angularSpeed * Time.deltaTime );
+			transform.rotation = newRotation;
+			cameraTransform.rotation = originalCameraRotation;
+
+			dir *= maxSpeed;
+			dir.y = rb.velocity.y;
+
+			rb.velocity = dir;
+		}
+		else
+		{
+			rb.velocity = Vector3.zero;
+		}
     }
 }

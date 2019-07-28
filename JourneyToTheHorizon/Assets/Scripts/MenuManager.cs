6d23152bb09b206b086ea9cf48c128e3d9f3216a
Dana Kimball <dkimball@ucsd.edu>
2019-07-27 17:35:49 -0700

    Added Main Menu and Esc Menu

---
+++ JourneyToTheHorizon/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MenuManager : MonoBehaviour
+{
+	[SerializeField] private GameObject escMenu;
+
+	private bool wasEscPressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if ( escMenu != null )
+		{
+			if ( Input.GetAxis( "Cancel" ) > 0 )
+			{
+				if ( !wasEscPressed )
+				{
+					escMenu.SetActive( !escMenu.activeSelf );
+				}
+				wasEscPressed = true;
+			}
+			else
+			{
+				wasEscPressed = false;
+			}
+		}
+    }
+
+	public void StartGame()
+	{
+		SceneManager.LoadSceneAsync( "Dana's Test Scene" );
+	}
+
+	public void ExitGame()
+	{
+#if UNITY_EDITOR
+		EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+}
